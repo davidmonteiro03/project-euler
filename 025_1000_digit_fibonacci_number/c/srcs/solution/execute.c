99e51a32ac8d07071af0646de10591f0228f9618
davidmonteiro03 <damonteiro12446@gmail.com>
2025-04-15 13:00:40 +0100

    up

---
+++ 025_1000_digit_fibonacci_number/c/srcs/solution/execute.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 10:13:55 by dcaetano          #+#    #+#             */
+/*   Updated: 2025/04/15 13:00:14 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/solve_c.h"
+
+static void	solve(long long int n)
+{
+	long long int	solution;
+
+	solution = 0;
+	fprintf(stdout, "Solution for %lld: %lld\n", n, solution);
+}
+
+void	solution_execute(t_solution *solution)
+{
+	long long int	n;
+	int				ret;
+
+	if (solution == NULL)
+		return ;
+	if (solution->argc != 2)
+	{
+		fprintf(stderr, "usage: %s <n>\n", solution->argv[0]);
+		g_exit_status = EX_USAGE;
+		return ;
+	}
+	n = 0;
+	ret = sscanf(solution->argv[1], "%lld", &n);
+	if (ret == -1)
+	{
+		fprintf(stderr, "%s: error: '%s' is not an integer.\n",
+			solution->argv[0], solution->argv[1]);
+		g_exit_status = EX_DATAERR;
+		return ;
+	}
+	solve(n);
+}
