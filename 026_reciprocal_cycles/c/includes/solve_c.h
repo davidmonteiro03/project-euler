a823e03ae20cac5a9bb5055c9e0a6d0a0e4ea3ce
davidmonteiro03 <damonteiro12446@gmail.com>
2025-04-15 19:32:32 +0100

    git updates

---
+++ 026_reciprocal_cycles/c/includes/solve_c.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve_c.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 09:41:55 by dcaetano          #+#    #+#             */
+/*   Updated: 2025/04/15 19:04:28 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOLVE_C_H
+# define SOLVE_C_H
+
+# include "solution.h"
+# include <stdio.h>
+# include <sysexits.h>
+
+extern int	g_exit_status;
+
+#endif
